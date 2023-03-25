bd3586989961de2e15c4c8852fe51820c2975334
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-03-25 09:50:58 +0530

    Add files via upload

---
+++ solidrectangle.java
@@ -0,0 +1,18 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+
+		int row=4;
+		int col=5;
+		System.out.println("Solid Rectangle");
+		for(int i=1;i<=row;i++)
+		{
+		    for(int j=0;j<=col;j++)
+		    {
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
