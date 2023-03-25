8644cad254a937be564947beb7a2e85c1273e763
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-03-25 10:37:29 +0530

    Update LeftTriangle.java

--- LeftTriangle.java
+++ LeftTriangle.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
+      System.out.print("Enter the number of rows you want to print: "); 
       Scanner obj=new Scanner(System.in);
-      System.out.println("input your number");
       int num=obj.nextInt();
       System.out.println("Left Triangle Star Pattern");
    for(int i=1;i<=num;i++)
