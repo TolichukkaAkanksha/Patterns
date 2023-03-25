38636914a86e86786000a392ba913f3975c6c20a
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-03-25 10:38:53 +0530

    Update solidrectangle.java

--- solidrectangle.java
+++ solidrectangle.java
@@ -2,10 +2,9 @@
 public class Main
 {
 	public static void main(String[] args) {
-
+                System.out.print("Enter the number of rows you want to print: ");
 		int row=4;
 		int col=5;
-		System.out.println("Solid Rectangle");
 		for(int i=1;i<=row;i++)
 		{
 		    for(int j=0;j<=col;j++)
